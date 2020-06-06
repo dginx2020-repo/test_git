467f4587b138c0a53e5440d4be41125a3792e14a
dginx2020-repo <66513218+dginx2020-repo@users.noreply.github.com>
2020-06-07 02:46:49 +0530

    Final version

--- executesc.sh
+++ executesc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Hello User"
-cat /dginx/scripts/author >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+echo "Hello User script success"
+
 
 echo "DATE: " >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 date > /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
@@ -15,5 +15,6 @@ git branch >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 echo "Last 5 commit:" >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 git log --oneline -5 >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 
+cat /dginx/scripts/author >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 cat /dginx/scripts/copyright >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
 
