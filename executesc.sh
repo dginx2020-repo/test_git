2221bbeedf3e7aad82b2eb1140d0664dbf35386a
siputestlab <52356011+siputestlab@users.noreply.github.com>
2020-06-07 00:17:53 +0530

    execution script added

---
+++ executesc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "DATE: " >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+date > /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+
+echo "UPTIME:" >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+uptime >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+
+echo "Current Branch:" >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+git branch >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+
+echo "Last 5 commit:" >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
+git log --oneline -5 >> /dginx/logs/repo/test1/"log-`date '+%Y-%m-%d-%I-%M-%p'`.txt"
